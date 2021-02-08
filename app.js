2e5e1a50492a80d158c4a2da0bd609662267bdb8
Bilboking <brkbendreality@gmail.com>
2021-02-08 06:22:11 -0600

    new branch

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
+
+//** EJS */
+app.use(expressLayouts);
+app.set('view engine', 'ejs')
+
+//** allow stylesheet access to ejs files */
+app.use(express.static(__dirname + '/public'));
+
+//** routes */
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 
