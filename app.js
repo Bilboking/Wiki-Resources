fa8e6b6d8fefb760bae7745bd3909c2154f79f1c
Bilboking <brkbendreality@gmail.com>
2021-02-09 15:49:38 -0600

    02-09-21

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//** dotenv DB connect */
+require('dotenv').config();
+mongoose.connect(process.env.DB_URI,  {
+    dbName: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+    .then( (res) => console.log('db connected'))
+    .catch((err) => console.log(err));
 
 //** EJS */
 app.use(expressLayouts);
--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,7 @@ router.get('/login', (req, res) => res.render('login'));
 //** Register page */
 router.get('/register', (req, res) => res.render('register'));
 
+//** Main page */
+router.get('/', (req, res) => res.render('welcome'));
+
 module.exports = router;
