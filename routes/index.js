36184c7a3561d2fd75f321a55a96799ad80d24fa
Bilboking <brkbendreality@gmail.com>
2021-02-14 14:57:50 -0600

    dashboard works

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth')
 
+//** Welcome page (not signed in) */
 router.get('/', (req, res) => res.render('welcome'));
+
+//** Dashboard (once signed in) */
+router.get('/dashboard', ensureAuthenticated, (req, res) => 
+    res.render('dashboard', {
+        email: req.user.email
+    }));
+
 
 module.exports = router;
